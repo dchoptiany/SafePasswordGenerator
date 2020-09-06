33c1f4e0495a333fcdb22b8f529ee5a952140383
Damian Choptiany <damianchoptiany@gmail.com>
2020-09-06 22:42:58 +0200

    Added dark mode

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
@@ -41,7 +42,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
@@ -83,11 +84,23 @@
             this.panel1.Size = new System.Drawing.Size(764, 126);
             this.panel1.TabIndex = 2;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Poppins", 9.5F);
+            this.label4.Location = new System.Drawing.Point(636, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "(G)";
+            // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button3.ForeColor = System.Drawing.Color.White;
@@ -104,6 +117,7 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.ForeColor = System.Drawing.Color.White;
@@ -236,15 +250,20 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label4
+            // button5
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(639, 50);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(30, 23);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "(G)";
+            this.button5.BackColor = System.Drawing.SystemColors.InfoText;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(697, 418);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 35);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Dark mode";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
@@ -252,6 +271,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
@@ -289,6 +309,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,43 +12,58 @@ namespace SafePasswordGenerator
 {
     public partial class Form1 : Form
     {
+        private bool darkMode = false;
+        
+        private Color lightBackgroundColor1 = Color.FromArgb(247, 236, 225);
+        private Color lightBackgroundColor2 = Color.FromArgb(90, 167, 90);
+        private Color lightTextColor1 = Color.FromArgb(35, 27, 30);
+        private Color lightTextColor2 = Color.White;
+
+        private Color darkBackgroundColor1 = Color.FromArgb(49, 54, 56);
+        private Color darkBackgroundColor2 = Color.FromArgb(33, 161, 121);
+        private Color darkTextColor1 = Color.FromArgb(234, 224, 204);
+        private Color darkTextColor2 = Color.White;
+
         public Form1()
         {
             InitializeComponent();
 
-            Color backgroundColor1 = Color.FromArgb(247, 236, 225);
-            Color backgroundColor2 = Color.FromArgb(90, 167, 90);
-            Color textColor1 = Color.FromArgb(35, 27, 30);
-            Color textColor2 = Color.White;
+            BackColor = lightBackgroundColor1;
 
-            BackColor = backgroundColor1;
+            label1.ForeColor = lightTextColor1;
+            label2.ForeColor = lightTextColor1;
+            label3.ForeColor = lightTextColor1;
+            label4.ForeColor = lightTextColor1;
 
-            label1.ForeColor = textColor1;
-            label2.ForeColor = textColor1;
-            label3.ForeColor = textColor1;
+            checkedListBox1.ForeColor = lightTextColor1;
+            checkedListBox1.BackColor = lightBackgroundColor1;
 
-            checkedListBox1.ForeColor = textColor1;
-            checkedListBox1.BackColor = backgroundColor1;
+            checkedListBox2.ForeColor = lightTextColor1;
+            checkedListBox2.BackColor = lightBackgroundColor1;
+
+            button1.ForeColor = lightTextColor2;
+            button1.BackColor = lightBackgroundColor2;
+
+            button3.ForeColor = lightTextColor2;
+            button3.BackColor = lightBackgroundColor2;
+
+            numericUpDown1.ForeColor = lightTextColor2;
+            numericUpDown1.BackColor = lightBackgroundColor2;
+
             checkedListBox1.SetItemChecked(0, true);
             checkedListBox1.SetItemChecked(1, true);
             checkedListBox1.SelectionMode = SelectionMode.None;
 
-            checkedListBox2.ForeColor = textColor1;
-            checkedListBox2.BackColor = backgroundColor1;
             checkedListBox2.SetItemChecked(0, true);
             checkedListBox2.SetItemChecked(1, true);
 
-            button1.ForeColor = textColor2;
-            button1.BackColor = backgroundColor2;
-
-            button3.ForeColor = textColor2;
-            button3.BackColor = backgroundColor2;
             button3.Enabled = false;
 
-            numericUpDown1.ForeColor = textColor2;
-            numericUpDown1.BackColor = backgroundColor2;
             numericUpDown1.Minimum = 6;
             numericUpDown1.Maximum = 32;
+
+            button5.ForeColor = darkTextColor1;
+            button5.BackColor = darkBackgroundColor1;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -78,6 +93,71 @@ namespace SafePasswordGenerator
             if(e.KeyCode.ToString() == "G")
             {
                 button1_Click(sender, e);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(!darkMode)
+            {
+                button5.Text = "Light mode";
+
+                BackColor = darkBackgroundColor1;
+
+                label1.ForeColor = darkTextColor1;
+                label2.ForeColor = darkTextColor1;
+                label3.ForeColor = darkTextColor1;
+                label4.ForeColor = darkTextColor1;
+
+                checkedListBox1.ForeColor = darkTextColor1;
+                checkedListBox1.BackColor = darkBackgroundColor1;
+
+                checkedListBox2.ForeColor = darkTextColor1;
+                checkedListBox2.BackColor = darkBackgroundColor1;
+
+                button1.ForeColor = darkTextColor2;
+                button1.BackColor = darkBackgroundColor2;
+
+                button3.ForeColor = darkTextColor2;
+                button3.BackColor = darkBackgroundColor2;
+
+                numericUpDown1.ForeColor = darkTextColor2;
+                numericUpDown1.BackColor = darkBackgroundColor2;
+                button5.ForeColor = lightTextColor1;
+                button5.BackColor = lightBackgroundColor1;
+
+                darkMode = true;
+            }
+            else
+            {
+                button5.Text = "Dark mode";
+
+                BackColor = lightBackgroundColor1;
+
+                label1.ForeColor = lightTextColor1;
+                label2.ForeColor = lightTextColor1;
+                label3.ForeColor = lightTextColor1;
+                label4.ForeColor = lightTextColor1;
+
+                checkedListBox1.ForeColor = lightTextColor1;
+                checkedListBox1.BackColor = lightBackgroundColor1;
+
+                checkedListBox2.ForeColor = lightTextColor1;
+                checkedListBox2.BackColor = lightBackgroundColor1;
+
+                button1.ForeColor = lightTextColor2;
+                button1.BackColor = lightBackgroundColor2;
+
+                button3.ForeColor = lightTextColor2;
+                button3.BackColor = lightBackgroundColor2;
+
+                numericUpDown1.ForeColor = lightTextColor2;
+                numericUpDown1.BackColor = lightBackgroundColor2;
+
+                button5.ForeColor = darkTextColor1;
+                button5.BackColor = darkBackgroundColor1;
+
+                darkMode = false;
             }
         }
     }
